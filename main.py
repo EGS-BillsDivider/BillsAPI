c0581cccaf2d0ed9cf066f821cb9030e599e7eba
JPCGameiro <joao.gameiro@ua.pt>
2022-04-04 23:55:11 +0100

    get all bills and get bill by id

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 #Run with: uvicorn main:app --reload
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import mysql.connector
+from credentials import myHost, myUser, myPassword
+from uuid import UUID
 
 app = FastAPI()
+
+#Set mysql access credentials
+mydb = mysql.connector.connect(
+  host=myHost,
+  user=myUser,
+  password=myPassword
+)
+#Cursor to access mysql
+mycursor = mydb.cursor()
+mycursor.execute("USE bills")
+
+
+
+
+
 
 @app.get("/")
 async def root():
@@ -11,12 +29,29 @@ async def root():
 
 @app.get("/bills")
 async def getbills():
-    return {"message" : "GET /bills good"}
+    mycursor.execute("SELECT * FROM BILLS")
+    res = []
+    for value in mycursor:
+        res.append({"billReceiver": int(value[1]) , "billPayer": int(value[2]), "payDate": str(value[3]), "payedValue": float(value[4])})  
+    return res
 
 
 @app.get("/bill/{billId}")
-async def getbills(billId):
-    return {"message" : billId}
+async def getbills(billId : str):
+    #Verify if billId is valid
+    try:
+        UUID(billId)
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Invalid bill id supplied")
+    
+    #Search for the bill and throw 404 if not found
+    select = "SELECT * FROM BILLS WHERE id = (%s)"
+    mycursor.execute(select, [billId])
+    value = mycursor.fetchone()
+    if value == None:
+        raise HTTPException(status_code=404, detail="Bill requested cannot be found in the system")
+    else:
+        return {"billReceiver": int(value[1]) , "billPayer": int(value[2]), "payDate": str(value[3]), "payedValue": float(value[4])}
 
 
 @app.post("/bill")
